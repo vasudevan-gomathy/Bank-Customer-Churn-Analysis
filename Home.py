3145e0157ccd8b946df57d6a61fd5bac7fed106c
Vasudevan <85822284+vasudevan-gomathy@users.noreply.github.com>
2022-11-21 18:26:37 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,33 @@
+# -----------------------------------------Importing Packages--------------------------------------
+import joblib
+import streamlit as st
+from streamlit_lottie import st_lottie
+import json
+import requests
+# -----------------------------------------Importing Packages--------------------------------------
+
+# --------------------------------------------Page Layout-------------------------------------------
+
+st.set_page_config(
+    page_title= 'Churn Predictor'
+)
+
+con = st.container()
+side = st.sidebar
+
+def lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+lottie_ani = lottieurl('https://assets9.lottiefiles.com/packages/lf20_EHugAD.json')
+ani = st_lottie(lottie_ani, key = "hello")
+
+title = con.title('Customer Churn Predictor')
+anim = con.ani
+head = con.header('This app will predict whether the customer with given set of details will churn or not.')
+score = con.text('Accuracy: 85.71%')
+
+# --------------------------------------------Page Layout-------------------------------------------
+
+
